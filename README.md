4b622caaab358d1975c76d2dbe5d95ae9fc024aa
h181221 <peder.wiig@gmail.com>
2018-01-10 23:42:01 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# CryptoScraperLite
--- src/main/java/no/pederyo/Scraper/ScrapeRunner.java
+++ src/main/java/no/pederyo/Scraper/ScrapeRunner.java
@@ -7,6 +7,9 @@ import java.net.MalformedURLException;
 
 public class ScrapeRunner implements Runnable {
     private String name;
+    private static double oldValue30SekSjekk = 0.0;
+    private static double forjeVerdi30minSjekk = 0.0;
+    private static int iterasjon = 0;
 
     public ScrapeRunner(String name) {
         this.name = name;
@@ -16,7 +19,12 @@ public class ScrapeRunner implements Runnable {
     public void run() {
         while(true){
             try {
+                iterasjon++;
                 Coin coin = CoinService.getPris(name);
+                double verdi = coin.getPris();
+                if(iterasjon % 3 == 0) {
+                    oldValue30SekSjekk = verdi;
+                }
                 System.out.println("Navn: " + coin.getName() + " Pris: " + coin.getPris());
                 Thread.sleep(3000);
             } catch (MalformedURLException e) {
@@ -27,3 +35,11 @@ public class ScrapeRunner implements Runnable {
         }
     }
 }
+/*
+
+        halvTimeSjekkVerdiEndring(coin, verdi); // sjekker verdi endring på IoTa
+
+        sjekkforjeVerdi(oldValue, verdi); // sjekker om det har skjedd en endring på 4% på 30 sekunder.
+
+        DagSammendrag(coin, verdi, avk);  // hver 24 time send summary.
+* */
